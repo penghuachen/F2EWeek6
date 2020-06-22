08b774958407c8ded63aa4265e3bb74e3192e048
Penghua <kids5346@gmail.com>
2020-06-22 22:08:03 +0800

    feat: publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/F2EWeek6/'
+    : '/',
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
 
